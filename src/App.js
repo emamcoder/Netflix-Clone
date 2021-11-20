7e199e7a5b6f6331c6f189b0ed71e32578382241
Emamul Hasan <emamulhasan@Emamuls-MacBook-Air.local>
2021-11-20 13:13:19 +0530

    stopwatch component added

--- src/App.js
+++ src/App.js
@@ -6,21 +6,24 @@ import Home from "./components/Home/Home";
 import MovieDEtail from "./components/MovieDetail/MovieDetail";
 import PageNotFound from "./components/PageNotFound/PageNotFound";
 import Footer from "./components/Footer/Footer";
+import StopWatch from './components/Stopwatch'
 
 function App() {
   return (
     <div className="app">
-      <Router>
+      {/* <Router>
         <Header></Header>
         <div className="container">
-        <Switch>
-        <Route path="/" component={Home} />
-        <Route path="/movie/:imdbID" component={MovieDEtail} />
-        <Route component={PageNotFound} />
-        </Switch>
+          <Switch>
+            <Route path="/" component={Home} />
+            <Route path="/movie/:imdbID" component={MovieDEtail} />
+            <Route component={PageNotFound} />
+            
+          </Switch>
         </div>
         <Footer />
-      </Router>
+      </Router> */}
+      <StopWatch />
     </div>
   );
 }
